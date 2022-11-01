05d8a2bf04a02e26d9bcd7e5b3fb222be3f52b7c
Hernan Sisi <Hernansisi2002@gmail.com>
2022-11-01 14:11:49 -0300

    v3.0

--- js/generadorTarjetas.js
+++ js/generadorTarjetas.js
@@ -7,14 +7,19 @@ export const generadorDeTarjetas = ({nombre,descripcion,precio,imagen}) => {
     imag.classList.add('tarjetas__imagen');
     imag.src = imagen;
     let agrupadorUno = document.createElement('div');
-    agrupadorUno.classList.add('tarjetas__agrupador');
+    agrupadorUno.classList.add('tarjetas__agrupador__sector1');
     agrupadorUno.appendChild(imag);
     agrupadorUno.appendChild(generadorInformacion(nombre,descripcion));
     tarjeta.appendChild(agrupadorUno);
     let agrupadorDos = document.createElement('div');
-    agrupadorDos.classList.add('tarjetas__agrupador');
-    agrupadorDos.appendChild(generadorDePrecio(precio));
-    agrupadorDos.appendChild(generarCantidad());
+    agrupadorDos.classList.add('tarjetas__agrupador__sector2');
+    agrupadorDos.appendChild(generarCantidadDisminuir());
+    let agrupadorPreciosCantidad = document.createElement('div');
+    agrupadorPreciosCantidad.classList.add('tarjetas__precios__contenedor');
+    agrupadorPreciosCantidad.appendChild(generadorDePrecio(precio));
+    agrupadorPreciosCantidad.appendChild(generarCantidad());
+    agrupadorDos.appendChild(agrupadorPreciosCantidad);
+    agrupadorDos.appendChild(generarCantidadAumentar())
     tarjeta.appendChild(agrupadorDos);
     return tarjeta;
 };
@@ -45,31 +50,44 @@ const generadorDePrecio = (precio) => {
     costo.appendChild(rebaja);
     return costo;
 };
+const generarCantidadAumentar = () => {
+    let aumentar = document.createElement('button');
+    aumentar.classList.add('tarjetas__btn__aumentar');
+    let sigMas = document.createElement('i');
+    sigMas.classList.add('fa-solid', 'fa-plus');
+    aumentar.appendChild(sigMas);
+    aumentar.addEventListener('click',btnAumentar);
+    return aumentar;
+};
+const generarCantidadDisminuir = () => {
+    let disminuir = document.createElement('button');
+    let sigMenos = document.createElement('i');
+    sigMenos.classList.add('fa-solid', 'fa-minus');
+    disminuir.appendChild(sigMenos);
+    disminuir.addEventListener('click',btnDisminuir);
+    disminuir.classList.add('tarjetas__btn__disminuir');
+    return disminuir;
+}
 const generarCantidad = () => {
     let cantidades = document.createElement('div');
     cantidades.classList.add('tarjetas__cantidad');
     let contador = document.createElement('span');
+    // contador.classList.
     contador.classList.add('tarjetas__inp__cantidad');
     contador.dataset.cantidad = 0;
     contador.textContent = 0;
     cantidades.appendChild(contador);
-    let botonera = document.createElement('div');
-    botonera.classList.add('tarjetas__btns');
-    let aumentar = document.createElement('button');
-    aumentar.classList.add('tarjetas__btn__aumentar');
-    aumentar.textContent = '+';
-    aumentar.addEventListener('click',btnAumentar);
-    botonera.appendChild(aumentar);
-    let disminuir = document.createElement('button');
-    disminuir.classList.add('tarjetas__btn__disminuir');
-    disminuir.textContent = '-';
-    disminuir.addEventListener('click',btnDisminuir);
-    botonera.appendChild(disminuir);
-    cantidades.appendChild(botonera);
     return cantidades;
 }
+
 const btnAumentar = (event) => {
-    let contador = event.path[2].querySelector('[data-cantidad]');
+    let avance = () => {
+        if (event.path[0].classList.value == 'tarjetas__btn__aumentar') {
+            return 2;
+        }
+        return 3;
+    }
+    let contador = event.path[avance()].querySelector('[data-cantidad]');
     if (contador.dataset.cantidad <99) {
         contador.dataset.cantidad++;
         contador.textContent = contador.dataset.cantidad;
@@ -77,7 +95,13 @@ const btnAumentar = (event) => {
     }
 };
 const btnDisminuir = (event) => {
-    let contador = event.path[2].querySelector('[data-cantidad]');
+    let avance = () => {
+        if (event.path[0].classList.value == 'tarjetas__btn__aumentar') {
+            return 2;
+        }
+        return 3;
+    }
+    let contador = event.path[avance()].querySelector('[data-cantidad]');
     if (contador.dataset.cantidad > 0) {
         contador.dataset.cantidad--;
         contador.textContent = contador.dataset.cantidad;
